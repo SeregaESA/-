023cdba65833c65ce93830918560bad35098b686
SeregaESA <esa_scb@mail.ru>
2024-08-08 15:34:41 +0300

    Add files via upload

---
+++ SeregaArab.java
@@ -0,0 +1,8 @@
+public enum SeregaArab {
+    I(1), II(2), III(3), IV(4), V(5), VI(6), VII(7), VIII(8), IX(9), X(10);
+
+    int translation;
+    private SeregaArab (int translation){this.translation = translation;}
+    public int getTranslation(){return translation;}
+}
+
